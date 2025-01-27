f117a7d31bea9fbf73091c6d55ea3bf86b6c1ef6
oskar <67971701+k4rdel@users.noreply.github.com>
2025-01-28 00:23:53 +0100

    Added a lot of things

--- app.py
+++ app.py
@@ -1,21 +1,68 @@
 import os
-from flask import Flask, render_template
-from forms import RegistrationForm
+from flask import Flask, render_template, redirect, url_for, flash
+from forms import RegistrationForm, LoginForm
+from models import db, User
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        # Zapisz dane do bazy danych
-        return redirect(url_for('home'))
+        hashed_password = generate_password_hash(form.password.data)
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash(f'Konto utworzone dla {form.username.data}!', 'success')
+        return redirect(url_for('register'))
     return render_template('register.html', form=form)
 
-def home():
-    return render_template('index.html')
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and check_password_hash(user.password, form.password.data):
+            login_user(user)
+            flash('Zalogowano pomyślnie!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Nieprawidłowy email lub hasło', 'danger')
+    return render_template('login.html', form=form)
 
-if __name__ == "__main__":
-    # Pobierz port z zmiennej środowiskowej lub ustaw domyślny na 8080
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('Wylogowano pomyślnie!', 'success')
+    return redirect(url_for('login'))
+
+
+@app.route('/home')
+@login_required
+def home():
+    return f'Witaj, {current_user.username}!'
+
+@app.route('/')
+def index():
+    return redirect(url_for('home'))
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
--- forms.py
+++ forms.py
@@ -1,7 +1,16 @@
+# filepath: /c:/Users/oskar/online-store/forms.py
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    username = StringField('Nazwa użytkownika', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Hasło', validators=[DataRequired()])
+    confirm = PasswordField('Powtórz hasło', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Zarejestruj')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Hasło', validators=[DataRequired()])
+    submit = SubmitField('Zaloguj')
