14744c23a3229446c8803578ca21767af5a97ea5
oskar <67971701+k4rdel@users.noreply.github.com>
2025-01-27 19:15:10 +0100

    Fix port binding for Render

--- app.py
+++ app.py
@@ -8,6 +8,6 @@ def home():
     return "Hello, Render!"
 
 if __name__ == "__main__":
-    # Pobranie portu z konfiguracji środowiska lub domyślnie 8080
+    # Pobierz port z zmiennej środowiskowej lub ustaw domyślny na 8080
     port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
