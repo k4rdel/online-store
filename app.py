24763cbd7cab8edd143f311bfeaca3fe8db90eae
oskar <67971701+k4rdel@users.noreply.github.com>
2025-01-27 19:13:39 +0100

    Opis zmian, np. Initial commit

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template
+import os
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('index.html')
+    return "Hello, Render!"
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    # Pobranie portu z konfiguracji środowiska lub domyślnie 8080
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
